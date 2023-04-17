e0ff5527282a588d28103cc963abcec10720bc8c
leuldawit <97153381+leuldawit@users.noreply.github.com>
2023-04-17 21:32:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,83 +1,83 @@
-import { WORDS } from './words.js';
+  import { WORDS } from './words.js';
 
-var submit = document.getElementById('submit');
-var win = document.getElementById('win');
-var help = document.getElementById('help');
-var close = document.getElementById('close');
+  var submit = document.getElementById('submit');
+  var win = document.getElementById('win');
+  var help = document.getElementById('help');
+  var close = document.getElementById('close');
 
-var word =getRandomItem(WORDS);
-var answer = word.split('');
-var pos = 1;
+  var word =getRandomItem(WORDS);
+  var answer = word.split('');
+  var pos = 1;
 
-console.log(word);
+  console.log(word);
 
-$('#submit').click(function() {
-  var textarea = document.getElementById('text').value;
-  var text = textarea.split('');
-  win.style.display='none'
-  
-  // checks if the word really exists or not
-  if(WORDS.includes(textarea)){  
+  $('#submit').click(function() {
+    var textarea = document.getElementById('text').value.toLowerCase();
+    var text = textarea.split('');
+    win.style.display='none'
     
-     if (pos > 25) {
-        win.textContent = 'game over!';
-        win.style.display = 'inline-block';
-        const text = document.getElementById('text');
-        text.parentNode.removeChild(text);
-        submit.parentNode.removeChild(submit);
+    // checks if the word really exists or not
+    if(WORDS.includes(textarea)){  
+      
+      if (pos > 25) {
+          win.textContent = 'game over!';
+          win.style.display = 'inline-block';
+          const text = document.getElementById('text');
+          text.parentNode.removeChild(text);
+          submit.parentNode.removeChild(submit);
 
-      } else {
-        text.forEach((t, index) => {
-          var label = document.getElementById(pos);
-          label.textContent = t;
-          pos++;
-          if (answer[index] === t) {
-            label.style.backgroundColor = '#6aaa64';// to display green 
-          } else if (answer.includes(t)) {
-            label.style.backgroundColor = '#c9b458';// to display yellow 
-          } else {
-            label.style.backgroundColor = '#787c7e';// to display grey 
-          }
-        });
+        } else {
+          text.forEach((t, index) => {
+            var label = document.getElementById(pos);
+            label.textContent = t;
+            pos++;
+            if (answer[index] === t) {
+              label.style.backgroundColor = '#6aaa64';// to display green 
+            } else if (answer.includes(t)) {
+              label.style.backgroundColor = '#c9b458';// to display yellow 
+            } else {
+              label.style.backgroundColor = '#787c7e';// to display grey 
             }
-      if (word === textarea) {
-        win.textContent = 'You Won!';
-        win.style.display = 'inline-block';
+          });
+              }
+        if (word === textarea) {
+          win.textContent = 'You Won!';
+          win.style.display = 'inline-block';
 
-        const text = document.getElementById('text');
-        text.parentNode.removeChild(text);
-        submit.parentNode.removeChild(submit);
-      }
+          const text = document.getElementById('text');
+          text.parentNode.removeChild(text);
+          submit.parentNode.removeChild(submit);
+        }
+    }
+    else{
+          win.textContent = 'Error: Invalid Word! Please Enter a valid word!';
+          win.style.display = 'inline-block';
+    }
+  });
+
+  $('#help').click(function() {
+    var inst = document.querySelector('.instructions');
+    inst.style.height = '60vh';
+  });
+
+  $('#close').click(function() {
+    var inst = document.querySelector('.instructions');
+    inst.style.height = '0vh';
+  });
+
+  $('#restart').click(function() {
+    // Refresh the page
+    location.reload();
+  });
+
+  function getRandomItem(WORDS) {
+
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * WORDS.length);
+
+    // get random item
+    const item = WORDS[randomIndex];
+
+    return item;
   }
-  else{
-        win.textContent = 'Error: Invalid Word! Please Enter a valid word!';
-        win.style.display = 'inline-block';
-  }
-});
-
-$('#help').click(function() {
-  var inst = document.querySelector('.instructions');
-  inst.style.height = '60vh';
-});
-
-$('#close').click(function() {
-  var inst = document.querySelector('.instructions');
-  inst.style.height = '0vh';
-});
-
-$('#restart').click(function() {
-  // Refresh the page
-  location.reload();
-});
-
-function getRandomItem(WORDS) {
-
-  // get random index value
-  const randomIndex = Math.floor(Math.random() * WORDS.length);
-
-  // get random item
-  const item = WORDS[randomIndex];
-
-  return item;
-}
 
